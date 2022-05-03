5101f4d540afc57ed9be9ce59a4822115e26f796
Davis Chang <davis.chang@langlive.com>
2022-05-03 15:05:57 +0800

    improve smart contract, and add event logs

--- contracts/SharedWallet.sol
+++ contracts/SharedWallet.sol
@@ -4,8 +4,8 @@ pragma solidity >=0.7.0 <0.9.0;
 
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/14ca3aeb798d9b9be31df86ae7ef8b8f760caa4c/contracts/access/Ownable.sol";
 
-contract SharedWallet is Ownable {
-    event Received(address, uint);
+contract Allowance is Ownable {
+    event AllowanceChanged(address indexed _forWho, address indexed _fromWho, uint _oldAmount, uint _newAmount);
     mapping(address => uint) public allowance;
 
     modifier ownerOrAllance(uint _amount) {
@@ -14,22 +14,30 @@ contract SharedWallet is Ownable {
     }
     
     function addAllowance(address _who, uint _amount) public onlyOwner {
+        emit AllowanceChanged(_who, msg.sender, allowance[_who], _amount);
         allowance[_who] = _amount;
     }
 
     function reduceAllowance(address _who, uint _amount) internal {
+        emit AllowanceChanged(_who, msg.sender, allowance[_who], allowance[_who] - _amount);
         allowance[_who] -= _amount;
     }
+}
+
+contract SharedWallet is Allowance {
+    event ReceivedMoney(address indexed _who, uint _amount);
+    event WithdrawnMoney(address indexed _from, address indexed _to, uint _amount);
 
     function withdrawMoney(address payable _to, uint _amount) public ownerOrAllance(_amount) {
         require(_amount <= address(this).balance, "There are not enough funds in this smart contract");
         if (owner() != msg.sender) {
             reduceAllowance(msg.sender, _amount);
         }
+        emit WithdrawnMoney(msg.sender, _to, _amount);
         _to.transfer(_amount);
     }
 
     receive() external payable {
-        emit Received(msg.sender, msg.value);
+        emit ReceivedMoney(msg.sender, msg.value);
     }
 }
