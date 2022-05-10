177fdfe9f444f3f3799c4bf1e9e6bbb2444ed051
Davis Chang <davis.chang@langlive.com>
2022-05-10 17:28:43 +0800

    add openzeppelin test-helpers into testing

--- dapps/tokenization/test/TutorialToken.test.js
+++ dapps/tokenization/test/TutorialToken.test.js
@@ -9,16 +9,47 @@ const { expect } = require('chai');
 
 const TutorialToken = artifacts.require("TutorialToken");
 
-contract("TutorialToken Test", accounts => {
-  it("all token should be in account", async () => {
+contract("TutorialToken", accounts => {
+
+  const [deployerAccount, recipient, anotherAccount] = accounts;
+
+  it("all tokens should be in account", async () => {
     console.log('test-helpers:', { ZERO_ADDRESS, BN1: new BN(1) });
     
     const instance = await TutorialToken.deployed();
     const totalSupply = await instance.totalSupply();
-    const balance = await instance.balanceOf(accounts[0]);
+    const balance = await instance.balanceOf(deployerAccount);
 
     // same test but different code 
     assert.equal(totalSupply.toNumber(), balance.toNumber(), "The balance was not the same");
     expect(totalSupply).to.be.bignumber.equal(balance);
   });
+
+
+  it("is possible to send tokens between accounts", async () => {
+    const sendToken = new BN(1);
+    const instance = await TutorialToken.deployed();
+    const totalSupply = await instance.totalSupply();
+
+    const receipt = await instance.transfer(recipient, sendToken);
+    expectEvent(receipt, 'Transfer', {
+      from: deployerAccount,
+      to: recipient,
+      value: sendToken,
+    });
+
+    expect(await instance.balanceOf(deployerAccount)).to.be.bignumber.equal(totalSupply.sub(sendToken));
+    expect(await instance.balanceOf(recipient)).to.be.bignumber.equal(sendToken);
+  })
+
+  it("is not posible to send more tokens than available in total", async () => {
+    const instance = await TutorialToken.deployed();
+    const deployerBalance = await instance.balanceOf(deployerAccount);
+
+    await expectRevert(
+      instance.transfer(recipient, new BN(deployerBalance + 1)),
+      'ERC20: transfer amount exceeds balance',
+    );
+    expect(await instance.balanceOf(deployerAccount)).to.be.bignumber.equal(deployerBalance);
+  })
 });
